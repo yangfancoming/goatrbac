00bb6d1f80cb62888e9a63123bc371beaf112b1f
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2020-02-27 09:09:30 +0800

    增加 左侧菜单动态显示

--- src/main/java/com/goat/rbac/goatrbac/system/controller/MenuController.java
+++ src/main/java/com/goat/rbac/goatrbac/system/controller/MenuController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
+
+
 @Controller
 public class MenuController extends BaseController {
 
