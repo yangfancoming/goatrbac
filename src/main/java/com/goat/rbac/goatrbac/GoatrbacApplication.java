c23f5a38a0bdb44442aa02b35b1607503b742711
yangfan <34465021+jwfl724168@users.noreply.github.com>
2020-09-08 15:12:24 +0800

    增加 tomcat部署功能

--- src/main/java/com/goat/rbac/goatrbac/GoatrbacApplication.java
+++ src/main/java/com/goat/rbac/goatrbac/GoatrbacApplication.java
@@ -3,13 +3,19 @@ package com.goat.rbac.goatrbac;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan({"com.goat.rbac.goatrbac.buzz.dao","com.goat.rbac.goatrbac.system.dao"})
-public class GoatrbacApplication {
+public class GoatrbacApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GoatrbacApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GoatrbacApplication.class);
+    }
 }
